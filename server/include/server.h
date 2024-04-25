910f8856af8c454490a5cdba6d0fc6165c1740fe
gabr <gabrpavel@gmail.com>
2024-04-25 21:23:51 +0300

    [refactoring] split a monolithic main.cpp into multiple files
    add server, error_handler, socket_utils, file_handler, command_handler

--- server/include/server.h
+++ server/include/server.h
@@ -1,7 +1,11 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include "socket.h"
+#include "ftp_commands.h"
 #include <iostream>
+#include <thread>
+#include <atomic>
 
 void run_server(int argc, char **argv);
 
--- server/src/server.cpp
+++ server/src/server.cpp
@@ -1,82 +1,73 @@
 #include "../include/server.h"
-#include "../include/socket.h"
-#include "../include/ftp_commands.h"
 
 void run_server(int argc, char **argv) {
+
     int listenfd, connfd, n;
     pid_t childpid;
     socklen_t clilen;
     char buf[MAXLINE];
     struct sockaddr_in cliaddr, servaddr;
 
-    if (argc !=2) {						//validating the input
-        cerr<<"Usage: ./a.out <port number>"<<endl;
+    if (argc !=3) {
+        std::cerr<<"Usage: ./server <port number> <root directory>"<<std::endl;
         exit(1);
     }
 
-    //Create a socket for the soclet
-    //If sockfd<0 there was an error in the creation of the socket
     if ((listenfd = socket (AF_INET, SOCK_STREAM, 0)) <0) {
-        cerr<<"Problem in creating the socket"<<endl;
+        std::cerr<<"Problem in creating the socket"<<std::endl;
         exit(2);
     }
 
-    //preparation of the socket address
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     if(atoi(argv[1])<=1024){
-        cerr<<"Port number must be greater than 1024"<<endl;
+        std::cerr<<"Port number must be greater than 1024"<<std::endl;
         exit(2);
     }
     servaddr.sin_port = htons(atoi(argv[1]));
 
-    //bind the socket
     bind (listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
-    //listen to the socket by creating a connection queue, then wait for clients
     listen (listenfd, LISTENQ);
 
-    cout<<"Server running...waiting for connections."<<endl;
+    std::cout<<"Server running...waiting for connections."<<std::endl;
 
     for ( ; ; ) {
 
         clilen = sizeof(cliaddr);
-        //accept a connection
         connfd = accept (listenfd, (struct sockaddr *) &cliaddr, &clilen);
 
-        cout<<"Received request..."<<endl;
+        std::cout<<"Received request..."<<std::endl;
 
-        if ( (childpid = fork ()) == 0 ) {//if it’s 0, it’s child process
+        if ( (childpid = fork ()) == 0 ) {
 
-            cout<<"Child created for dealing with client requests"<<endl;
+            std::cout<<"Child created for dealing with client requests"<<std::endl;
 
-            //close listening socket
             close (listenfd);
-            int data_port=1024;						//for data connection
+            int data_port=1024;
+
             while ( (n = recv(connfd, buf, MAXLINE,0)) > 0)  {
-                cout<<"String received from client: "<<buf;
-                // Here we will call the appropriate FTP command function based on the received command
+                std::cout<<"String received from client: "<<buf;
                 if (strncmp("ls", buf, 2) == 0) {
                     handle_ls_command(connfd, data_port);
                 } else if (strncmp("pwd", buf, 3) == 0) {
                     handle_pwd_command(connfd);
                 } else if (strncmp("cd", buf, 2) == 0) {
-                    handle_cd_command(connfd, buf+3);
+                    handle_cd_command(connfd, buf + 3);
                 } else if (strncmp("put", buf, 3) == 0) {
-                    handle_put_command(connfd, data_port, buf+4);
+                    handle_put_command(connfd, data_port, buf + 4);
                 } else if (strncmp("get", buf, 3) == 0) {
-                    handle_get_command(connfd, data_port, buf+4);
+                    handle_get_command(connfd, data_port, buf + 4);
                 } else {
-                    cout<<"Invalid command received: " << buf << endl;
+                    std::cout << "Invalid command received: " << buf << std::endl;
                 }
             }
 
             if (n < 0)
-                cout<<"Read error"<<endl;
+                std::cout<<"Read error"<<std::endl;
 
             exit(0);
         }
-        //close socket of the server
         close(connfd);
     }
 }
