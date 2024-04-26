5bc3373fd7d2536e87c29b40ba92eee77feda9c1
wupdp <kirillbond2017@gmail.com>
2024-04-27 02:54:59 +0300

    add ssl connection + add ftp_handler

--- server/include/server.h
+++ server/include/server.h
@@ -1,11 +1,22 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+
+
 #include "socket.h"
 #include "ftp_commands.h"
 #include <iostream>
 #include <thread>
 #include <atomic>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 void run_server(int argc, char **argv);
 
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,6 +1,8 @@
 #include "../include/server.h"
 
+void run_server_ssl(int argc, char **argv);
+
 int main(int argc, char **argv) {
-    run_server(argc, argv);
+    run_server_ssl(argc, argv);
     return 0;
 }
